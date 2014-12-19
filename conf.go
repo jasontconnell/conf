04eddfcf0d5947cc9d69fef1c4c5b67556950a41
Jason Connell <jconnell@delphicdigital.com>
2014-12-19 17:59:45 -0500

    Get jtccom header images working

---
+++ conf.go
@@ -0,0 +1,23 @@
+package conf
+
+import (
+	"os"
+	"fmt"
+	"encoding/json"
+)
+
+type Conf struct{}
+
+func LoadConfig(configFile string, conf interface{}) {
+	file, err := os.Open(configFile)
+ 
+	if err != nil {
+		fmt.Println("Error opening json file ", err, configFile)
+	}
+ 
+	decoder := json.NewDecoder(file)
+	err2 := decoder.Decode(conf)
+	if err2 != nil {
+		fmt.Println("error decoding json file", err);
+	}
+}
