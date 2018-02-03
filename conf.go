5c60278e9253a4b2ec49eb46589a27a23e084b2f
Jason Connell <jconnell@delphicdigital.com>
2018-02-03 01:04:49 -0500

    Better error handling, allow loading config from url

--- conf.go
+++ conf.go
@@ -4,20 +4,40 @@ import (
 	"os"
 	"fmt"
 	"encoding/json"
+	"net/http"
+	"io"
+	"errors"
 )
 
 type Conf struct{}
 
-func LoadConfig(configFile string, conf interface{}) {
+func LoadConfig(configFile string, conf interface{}) error {
 	file, err := os.Open(configFile)
  
 	if err != nil {
 		fmt.Println("Error opening json file ", err, configFile)
 	}
  
-	decoder := json.NewDecoder(file)
-	err2 := decoder.Decode(conf)
-	if err2 != nil {
-		fmt.Println("error decoding json file", err2, configFile);
+ 	return fromReader(file, conf)
+}
+
+func FromUrl(configUrl string, conf interface{}) error {
+	resp, err := http.Get(configUrl)
+
+	if err != nil {
+		return err
 	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return errors.New("couldn't get url: " + configUrl)
+	}
+
+	return fromReader(resp.Body, conf)
+}
+
+func fromReader(r io.Reader, conf interface{}) error {
+	decoder := json.NewDecoder(r)
+	return decoder.Decode(conf)
 }
